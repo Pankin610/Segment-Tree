edb613cc6899318ca8229702900652540cd0939f
Pankin610 <spdeveloper90@gmail.com>
2021-02-06 20:10:23 +0100

    testing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "SegmentTree.h"
 
 constexpr int(*un)(int, int) = [](int a, int b){ return std::max(a, b); };
+constexpr int(*sum)(int, int) = [](int a, int b){ return a + b; };
 
 int main() {
 
@@ -13,5 +14,10 @@ int main() {
   algo::SegmentTree<int, un, INT_MIN> st2(0, 50);
   st = st2;
   std::cout << st.Get(2, 5) << '\n';
+  algo::SegmentTree<int, sum, 0> sm(0, 5);
+  for (int i = 0; i < 5; i++) {
+    sm[i] = i;
+  }
+  std::cout << sm.Get(0, 4) << '\n';
   return 0;
 }
